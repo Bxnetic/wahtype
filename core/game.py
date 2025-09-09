f1b54db5ff72d3ec5609b5026c62c082f3de83ef
James Weithers <james.weithers@outlook.com>
2025-09-09 14:49:51 +0100

    Added home button and game_paused condition

--- core/game.py
+++ core/game.py
@@ -10,16 +10,21 @@ class Game:
 
         # screen, fps and run
         self.running = True # main game loop
+        self.game_paused = True
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE) # sets size of the window
         self.clock = pygame.time.Clock() # creates time using time pygame clock module
 
         # images for all the buttons
         self.reset_img = pygame.image.load("images\\reset_button.png").convert_alpha()
         self.reset_img_hover = pygame.image.load("images\\reset_button_hover.png").convert_alpha()
+        self.home_img = pygame.image.load("images\\home_button.png").convert_alpha()
+        self.home_img_hover = pygame.image.load("images\\home_button_hover.png").convert_alpha()
         
         # initiate buttons
         self.reset_button = Button(0, 0, self.reset_img, 
-             self.reset_img_hover, 0.2)
+            self.reset_img_hover, 0.2)
+        self.home_button = Button(0, 0, self.home_img,
+            self.home_img_hover, 0.2)
 
         # theme colours
         self.bgcolour = pygame.Color(BACKGROUND)
@@ -36,8 +41,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False # sets main game loop to false
-                elif event.type == pygame.KEYDOWN: # if presses any key, then add character to string
-                    self.text.text_handle(event)
+                if self.game_paused == False: # if the game is currently paused
+                    if event.type == pygame.KEYDOWN: # if presses any key, then add character to string
+                        self.text.text_handle(event)
 
             """ size of window """
             # get current width & height of window
@@ -53,19 +59,28 @@ class Game:
                 # set height back to 600, keep current width
 
             """ game elements """
-            self.screen.fill(self.bgcolour) # sets the display background to selected background colour
-            self.text.draw_text(self.width, self.height) # draws to screen and passes current width to draw_text
-              
-            # buttons
-            if self.text.done: # if the test has been completed
-                self.reset_button.rect.topleft = (int((self.width / 2)) - int(self.reset_button.rect.width / 2),
-                int((self.height / 2)) + 150) # move the button further down the screen
-            else:
-                 self.reset_button.rect.topleft = (int((self.width / 2)) - int(self.reset_button.rect.width / 2),
-                int((self.height / 2)) + 50) # set the positioning of the button depending on the current width & height
+            if self.game_paused == False: # if the game is currently paused
+                self.screen.fill(self.bgcolour) # sets the display background to selected background colour
+                self.text.draw_text(self.width, self.height) # draws to screen and passes current width to draw_text
+                
+                # buttons
+                if self.text.done: # if the test has been completed
+                    # move the buttons further down the screen
+                    self.reset_button.rect.topleft = (int((self.width / 2)) - int(self.reset_button.rect.width / 2) + 30,
+                    int((self.height / 2)) + 150)
+                    self.home_button.rect.topleft = (int((self.width / 2)) - int(self.home_button.rect.width / 2) - 30,
+                    int((self.height / 2)) + 150)
+                else:
+                    # set the positioning of the buttons depending on the current width & height
+                    self.reset_button.rect.topleft = (int((self.width / 2)) - int(self.reset_button.rect.width / 2) + 30,
+                    int((self.height / 2)) + 50) 
+                    self.home_button.rect.topleft = (int((self.width / 2)) - int(self.home_button.rect.width / 2) - 30,
+                    int((self.height / 2)) + 50)
 
-            if self.reset_button.draw(self.screen): # if the button is clicked
-                self.text.reset()
+                if self.reset_button.draw(self.screen): # if the button is clicked
+                    self.text.reset()
+                if self.home_button.draw(self.screen):
+                    pass
 
             pygame.display.flip() # continuously updates the screen
 
