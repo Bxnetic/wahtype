d931c8c8835eb78819b638fa6bfb69ca5b2fcfb1
Bxnetic <james.weithers@outlook.com>
2026-01-26 15:14:27 +0000

    Removed unused `centre` function

--- core/menu.py
+++ core/menu.py
@@ -1,6 +1,7 @@
 import pygame
 from config import *
 from ui.button import *
+from ui.surface_handle import *
 
 class Menu:
     def __init__(self, screen):
@@ -56,35 +57,30 @@ class Menu:
             self.green_button_hover_img, 0.55, "Start", 25, self.maintext, self.maintext) # button that displays "Start" (off screen)
 
     def draw(self, width, height, mouse_released):
-        def centre(surface, widthPadding, heightPadding): # function that centres surface
-            rect = surface.get_rect()
-            return (
-                int((width / 2)) - int(rect.width / 2) + widthPadding,
-             int((height / 2)) - int(rect.height / 2) + heightPadding
-            ) # centre surface
-
         self.screen.fill(self.bgcolour) # clear all the entities on screen
+
         # game icon
         self.game_img_rect = self.game_img.get_rect(
-            topleft=(centre(self.game_img, 0, -200))) # game icon rect
+            topleft = centre(self.game_img, width, height, 0, -200)) # game icon rect
         self.screen.blit(self.game_img, self.game_img_rect) # display game image on screen
+
         # buttons
-        self.play_button.rect.topleft = (centre(self.play_button.image, 0, -40)) # centre the button
+        self.play_button.rect.topleft = (centre(self.play_button.image, width, height, 0, -40,)) # centre the button
         if self.play_button.draw(self.screen, mouse_released): # if the play button is clicked
             return "selection"
-        self.stats_button.rect.topleft = (centre(self.stats_button.image, 0, 40)) # centre the button
+        self.stats_button.rect.topleft = (centre(self.stats_button.image, width, height, 0, 40)) # centre the button
         if self.stats_button.draw(self.screen, mouse_released): # if the scores button is clicked
             return "stats"
-        self.settings_button.rect.topleft = (centre(self.settings_button.image, 0, 120)) # centre the button
+        self.settings_button.rect.topleft = (centre(self.settings_button.image, width, height, 0, 120)) # centre the button
         if self.settings_button.draw(self.screen, mouse_released): # if the settings button is clicked
             return "settings"
-        self.quit_button.rect.topleft = (centre(self.quit_button.image, 0, 200)) # centre the button
+        self.quit_button.rect.topleft = (centre(self.quit_button.image, width, height, 0, 200)) # centre the button
         if self.quit_button.draw(self.screen, mouse_released): # if the quit button is clicked
             return "quit"
-        self.about_button.rect.topleft = (centre(self.blank_img, 0, 280)) # centre the button
+        self.about_button.rect.topleft = (centre(self.blank_img, width, height, 0, 280)) # centre the button
         if self.about_button.draw(self.screen, mouse_released): # if the quit button is clicked
             return "about"
-        if self.home_button.draw(self.screen, mouse_released):
+        if self.home_button.draw(self.screen, mouse_released): # if home button is clicked (pre-displayed)
             return "menu"
-        if self.start_button.draw(self.screen, mouse_released):
+        if self.start_button.draw(self.screen, mouse_released): # if start button is clicked (pre-displayed)
             return "game"
